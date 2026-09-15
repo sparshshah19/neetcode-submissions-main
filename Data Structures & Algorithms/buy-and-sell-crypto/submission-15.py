3db79ea2f7421750a76a5cb87cebf96063698e2c
Sparsh Shah <sparsh.shah@uwaterloo.ca>
2026-09-15 00:30:02 -0400

    Add: buy-and-sell-crypto - submission-15

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-15.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        max_profit = 0 
+
+        left = 0 
+        right = 1
+
+        while right < len(prices): 
+            if prices[right] - prices[left] > 0: 
+                profit = prices[right] - prices[left]
+                max_profit = max(max_profit, profit)
+            else: 
+                left = right 
+            right += 1
+        return max_profit 
